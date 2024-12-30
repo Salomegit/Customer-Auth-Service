d67684cd8effbca6cbfb9c3f5e92847ee5ef7154
Salomegit <sallygithinji72@gmail.com>
2024-12-30 10:10:46 +0300

    Add Chakra UI integration and update login component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 
+import { ChakraProvider } from '@chakra-ui/react'  
 import Login_route from './routes/auth/login_route'
 function App() {
 
   return (
-    <>
+    <ChakraProvider>
     <Login_route/>
-    </>
+    </ChakraProvider>
   )
 }
 
--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -1,6 +1,36 @@
+import { VStack, Formcontrol, Formlabel, Input,Button } from "@chakra-ui/react";
+import { Button, Fieldset, Input, Stack } from "@chakra-ui/react"
+
+
+import { useState } from "react";
 const Login = () => {
+
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('');
+    // const handleSubmit () ={ pa
+
+    // }
     return (
-        <h1>Login</h1>
+        <VStack>
+            {/* <Formcontrol>
+                <Formlabel>Username</Formlabel>
+                <Input onChange={(e) => setUsername(e.target.value)} value={username} type="text" />  
+                <Formlabel>Password</Formlabel>
+                <Input onChange={(e) => setPassword(e.target.value)} value={password} type="password" />  
+                <Button >Login</Button>
+            </Formcontrol> */}
+
+
+
+            <Field label="Name">
+          <Input name="name" />
+        </Field>
+
+        <Field label="Email address">
+          <Input name="email" type="email" />
+        </Field>
+
+        </VStack>
     )
     
     }
